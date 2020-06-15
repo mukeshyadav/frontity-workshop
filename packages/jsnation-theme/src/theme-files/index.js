133df41522624d2a453a006843426952d1f13ed6
Mukesh Yadav <mukeshyadav@gmail.com>
2020-06-15 23:37:36 +0530

    calling list, page and post component on main page

--- packages/jsnation-theme/src/theme-files/index.js
+++ packages/jsnation-theme/src/theme-files/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { connect } from "frontity";
 import Link from "./links";
+import List from "./list";
+import Post from "./post";
+import Page from "./page";
 
 const Root = ({ state }) => {
+  const data = state.source.get(state.router.link);
   return (
     <>
       <h1>Workshop</h1>
@@ -12,6 +16,12 @@ const Root = ({ state }) => {
         <Link href="/page/2">More Posts</Link>
         <Link href="/lorem-ipsum">Lorem Ipsum</Link>
       </nav>
+      <hr />
+      <main>
+        {data.isArchive && <List />}
+        {data.isPost && <Post />}
+        {data.isPage && <Page />}
+      </main>
     </>
   );
 };
