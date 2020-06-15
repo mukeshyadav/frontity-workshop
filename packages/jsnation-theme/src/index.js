734dd2eb0da38a5ccbca40391eb333d5a4cc040f
Mukesh Yadav <mukeshyadav@gmail.com>
2020-06-15 22:40:51 +0530

    adding navigation

--- packages/jsnation-theme/src/index.js
+++ packages/jsnation-theme/src/index.js
@@ -1,13 +1,4 @@
-import React from "react";
 import Root from "./theme-files";
-
-// const Root = () => {
-//   return (
-//     <>
-//       <h1>Frontity Workshop</h1>
-//     </>
-//   );
-// };
 
 export default {
   name: "jsnation-theme",
--- packages/jsnation-theme/src/theme-files/index.js
+++ packages/jsnation-theme/src/theme-files/index.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { connect } from "frontity";
+import Link from "./links";
 
-const Root = () => {
+const Root = ({ state }) => {
   return (
     <>
-      <h1>Component</h1>
+      <h1>Workshop</h1>
+      <p>URL: {state.router.link}</p>
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/page/2">More Posts</Link>
+        <Link href="lorem-ipsum">Lorem Ipsum</Link>
+      </nav>
     </>
   );
 };
 
-export default Root;
+export default connect(Root);
